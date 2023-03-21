bbf991f66d05ae9b5f4c292ebfe33fd0a7e794f7
Thomas Josef Dorfer <2110738536@stud.fh-kufstein.ac.at>
2023-03-21 22:04:33 +0100

    added create customer bulk test

---
+++ src/tests/create/create-customer-bulk.ts
@@ -0,0 +1,39 @@
+import { createTestData } from "@core/functions/create-test-data.function";
+import { AMOUNT_OF_BULK_ENTRIES } from "../../core/global.const";
+import { ICustomer } from "@core/models/entities/customer.model";
+import { ApiHttpClient } from "../../utils/api-http-client";
+import { DataStorage } from "../../utils/data-storage";
+import { FileWriter } from "../../utils/file-writer";
+import { Framework } from "../../core/framework";
+import { Test } from "../../models/test.model";
+import { DbSize } from "../../core/db-size";
+import { Db } from "../../core/db";
+
+export class CreateCustomerBulk extends Test<ICustomer> {
+    readonly endpoint = "customer/bulk";
+    readonly method = "create";
+
+    constructor(config: { dbSize: DbSize, db: Db, framework: Framework }) {
+        super("create-customer-bulk", config.dbSize, config.db, config.framework);
+    }
+
+    async exec(): Promise<void> {
+        await super.exec();
+
+        const data = DataStorage.instance.get(this);
+
+        for (let i = 0; i < 10; i++) {
+            const response = await ApiHttpClient.instance.post(this.endpoint, data);
+            if (response) {
+                this.results.push(response)
+            }
+        }
+
+        FileWriter.write(this);
+    }
+
+    createData(): any {
+        return createTestData.create.customers(AMOUNT_OF_BULK_ENTRIES);
+    }
+}
+
